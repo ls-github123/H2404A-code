9cd6f1709a35b000cde614dcb5808fd2a64b9ad9
神神&兔兔 <77359599+ls-github123@users.noreply.github.com>
2024-06-14 08:49:09 +0800

    Update # 5.录入身高数据，利用高阶函数排序输出.py

--- day12（函数进阶）/# 5.录入身高数据，利用高阶函数排序输出.py
+++ day12（函数进阶）/# 5.录入身高数据，利用高阶函数排序输出.py
@@ -4,7 +4,12 @@
  	# 	1.75 1.56 1.98 1.73 1.69 1.82 1.84
  	# 输出：
     #   1.98 1.84 1.82 1.75 1.73 1.69 1.56
-
-list_inptu = [1.75, 1.56, 1.98, 1.73, 1.69, 1.82, 1.84]
-ret = sorted(list_inptu, reverse = True)
-print(f'身高数据高阶排序后输出为:{ret}')
+heigh_list = []
+while True:
+    HeighInput = float(input('请输入身高数据(输入0结束):'))
+    if HeighInput == 0:
+        break
+    else:
+        heigh_list.append(HeighInput)
+ret = sorted(heigh_list, reverse = True)
+print(f'整合后的数据输出为:{ret}')
