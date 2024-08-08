e90e9f9fb482e593a2d3873b448aeba4533b7789
神神&兔兔 <77359599+ls-github123@users.noreply.github.com>
2024-08-08 08:46:26 +0800

    Update serializers.py

--- Django/p2-2/DRF(Django_RestFramework)/模型序列化器案例/index_serializer/engineer/serializers.py
+++ Django/p2-2/DRF(Django_RestFramework)/模型序列化器案例/index_serializer/engineer/serializers.py
@@ -1,8 +1,8 @@
 from wsgiref.validate import validator
-from . import models
+from 。 import models
 from rest_framework import serializers
 
-class EngiSerializer(serializers.Serializer):
+class EngiSerializer(serializers.ModelSerializer):
     # required=False 将所属字段设置为非必填项
     id = serializers.CharField(required=False)
     name = serializers.CharField() # 工程师姓名
@@ -14,17 +14,17 @@ class EngiSerializer(serializers.Serializer):
         model = models.EngineerModel
         fields = "__all__" # 返回所有字段
     
-    # 模型序列器无法自动执行create()操作
-    # 手动重写create()方法
-    def create(self, validated_data):
-        return models.EngineerModel.objects.create(**validated_data)
+    # # 模型序列器无法自动执行create()操作
+    # # 手动重写create()方法
+    # def create(self, validated_data):
+    #     return models.EngineerModel.objects.create(**validated_data)
     
-    # 模型序列化器无法自动执行update()操作
-    # 手动重写update()方法
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.age = validated_data.get('age', instance.age)
-        instance.professional = validated_data.get('professional', instance.professional)
-        instance.descinfo = validated_data.get('descinfo', instance.descinfo)
-        instance.save()
-        return instance
+    # # 模型序列化器无法自动执行update()操作
+    # # 手动重写update()方法
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.age = validated_data.get('age', instance.age)
+    #     instance.professional = validated_data.get('professional', instance.professional)
+    #     instance.descinfo = validated_data.get('descinfo', instance.descinfo)
+    #     instance.save()
+    #     return instance
